e4ca1ecca5cb5e49140f53376eef797a8cf05aa7
nanda kishore <nk732100@gmail.com>
2021-01-25 02:50:20 +0530

    A footer added.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,14 +2,16 @@
   <div class="home">
     <heading-component />
     <canvas-component />
+    <footer-component />
   </div>
 </template>
 
 <script>
 import HeadingComponent from "./heading-component.vue";
 import CanvasComponent from "./canvas-component.vue";
+import FooterComponent from "./footer-component";
 
 export default {
-  components: { HeadingComponent, CanvasComponent },
+  components: { HeadingComponent, CanvasComponent, FooterComponent },
 };
 </script>
---
+++ src/views/footer-component.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="container bg-dark">
+    <footer class="bg-dark navbar columns col-10 col-mx-auto">
+      <section class="navbar-section">
+        <span class="label label-primary"
+          >CSS Library used:
+          <a
+            href="https://picturepan2.github.io/spectre/index.html"
+            class="btn btn-link text-light"
+            >Spectre css</a
+          ></span
+        >
+      </section>
+      <section class="navbar-section">
+        <span class="label label-primary"
+          >Crypto-currency API:
+          <a href="https://coinranking.com/" class="btn btn-link text-light"
+            >Coinranking</a
+          ></span
+        >
+      </section>
+      <section class="navbar-section">
+        <span class="label label-primary"
+          >Vue Pagination Module:
+          <a
+            href="https://github.com/cornflourblue/jw-vue-pagination/tree/master/src"
+            class="btn btn-link text-light"
+            >jw-vue-pagination</a
+          ></span
+        >
+      </section>
+    </footer>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "footer-component",
+};
+</script>
+
+<style scope>
+footer {
+  margin-top: 20px;
+  margin-bottom: 20px;
+  padding: 2%;
+}
+</style>
